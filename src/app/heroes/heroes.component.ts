c124c5500e15119af1bdff5c36ba53c67dd3f105
Eugenio Navajo <eugenio.navasan@gmail.com>
2024-07-02 13:17:22 +0200

    tour of heroes, 3rd step

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  standalone: true,
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css',
+  imports : [ FormsModule, NgFor, NgIf, UpperCasePipe],
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
