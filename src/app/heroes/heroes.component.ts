9dcf06beebaa3532f113e83d5c0a76e569c63c3d
Eugenio Navajo <eugenio.navasan@gmail.com>
2024-07-02 15:32:53 +0200

    part 3 done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { NgFor} from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
-  imports : [ FormsModule, NgFor, NgIf, UpperCasePipe],
+  imports : [ NgFor , HeroDetailComponent],
 })
 export class HeroesComponent {
   heroes = HEROES;
